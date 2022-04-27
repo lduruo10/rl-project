b015c7c391a71f62e5bd340853742f618d5df75e
Erik Pettersson <erik@sublime.se>
2022-04-27 21:38:49 +0200

    Cartpole-v0 works. Pong is runnable at least.

---
+++ erik/config.py
@@ -0,0 +1,80 @@
+"""
+In this file, you may edit the hyperparameters used for different environments.
+
+memory_size: Maximum size of the replay memory.
+n_episodes: Number of episodes to train for.
+batch_size: Batch size used for training DQN.
+target_update_frequency: How often to update the target network.
+train_frequency: How often to train the DQN.
+gamma: Discount factor.
+lr: Learning rate used for optimizer.
+eps_start: Starting value for epsilon (linear annealing).
+eps_end: Final value for epsilon (linear annealing).
+anneal_length: How many steps to anneal epsilon for.
+n_actions: The number of actions can easily be accessed with env.action_space.n, but we do
+    some manual engineering to account for the fact that Pong has duplicate actions.
+"""
+
+# Hyperparameters for CartPole-v0
+CartPole_v0 = {
+    'memory_size': 10000,
+    'replay_start_size':32,
+    'n_episodes': 1000,
+    'batch_size': 32,
+    'target_update_frequency': 20,
+    'train_frequency': 1,
+    'gamma': 0.99,
+    'lr': 1e-4,
+    'eps_start': 1.0,
+    'eps_end': 0.01,
+    'anneal_length': 10**4,
+    'n_actions': 2
+}
+# Hyperparameters for CartPole-v1
+CartPole_v1 = {
+    'memory_size': 20000,
+    'replay_start_size':32,
+    'n_episodes': 1000,
+    'batch_size': 32,
+    'target_update_frequency': 40,
+    'train_frequency': 1,
+    'gamma': 0.98,
+    'lr': 1e-4,
+    'eps_start': 1.0,
+    'eps_end': 0.01,
+    'anneal_length': 10**4,
+    'n_actions': 2,
+}
+# Hyperparameters for Pong-v0
+Pong_v0 = {
+  'memory_size': 10000,
+  'replay_start_size':2000,
+  'n_episodes': 10000,
+  'batch_size': 32,
+  'target_update_frequency': 1000,
+  'train_frequency': 4,
+  'gamma': 0.99,
+  'lr': 1e-4,
+  'eps_start': 1.0,
+  'eps_end': 0.01,
+  'anneal_length': 10**6,
+  'n_actions': 2,
+  'obs_stack_size':4,
+  'screen_size':84
+}
+Pong_v4 = {
+  'memory_size': 10000,
+  'replay_start_size':2000,
+  'n_episodes': 10000,
+  'batch_size': 32,
+  'target_update_frequency': 1000,
+  'train_frequency': 4,
+  'gamma': 0.99,
+  'lr': 1e-4,
+  'eps_start': 1.0,
+  'eps_end': 0.01,
+  'anneal_length': 10**6,
+  'n_actions': 2,
+  'obs_stack_size':4,
+  'screen_size':84
+}
